8ecfd35bc67a5ce3e8df815dd646ae9987d8ff73
Olivier Girardot <ssaboum@gmail.com>
2014-03-29 11:35:09 +0100

    add: mock entry point for login

--- src/main/java/io/github/eneid/auth/LoginResource.java
+++ src/main/java/io/github/eneid/auth/LoginResource.java
@@ -1,7 +1,21 @@
 package io.github.eneid.auth;
 
-/**
- * Created by ogirardot on 29/03/2014.
- */
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
 public class LoginResource {
+
+    @RequestMapping(
+            value = {"", "/"},
+            method = RequestMethod.GET
+    )
+    public String logMeIn(@RequestParam("login") String login,
+                          @RequestParam("password") String password) {
+        
+        return "login " + login + " passwd " + password;
+    }
 }
