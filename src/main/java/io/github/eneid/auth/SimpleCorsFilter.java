f9bfca236e5bddc090506ad833792085fa0ba9af
Florent Biville <florent.biville@gmail.com>
2014-03-29 23:53:17 +0100

    Allow Content-Type

--- src/main/java/io/github/eneid/auth/SimpleCorsFilter.java
+++ src/main/java/io/github/eneid/auth/SimpleCorsFilter.java
@@ -14,7 +14,7 @@ public class SimpleCorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Authorization");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Authorization, Content-Type");
         chain.doFilter(req, res);
     }
     public void init(FilterConfig filterConfig) {}
