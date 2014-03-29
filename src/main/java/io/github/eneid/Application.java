7919084112467cacb151b821cf9873ee126bf753
Florent Biville <florent.biville@gmail.com>
2014-03-29 19:07:09 +0100

    Customize for Heroku
     1. add Procfile
     2. detect port at runtime

--- src/main/java/io/github/eneid/Application.java
+++ src/main/java/io/github/eneid/Application.java
@@ -2,6 +2,8 @@ package io.github.eneid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -23,7 +25,17 @@ public class Application {
                 .build();
     }
 
+    @Bean
+    public EmbeddedServletContainerFactory embeddedServletContainerFactory() {
+        return new TomcatEmbeddedServletContainerFactory(detectPort());
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    private static int detectPort() {
+        String port = System.getenv("PORT");
+        return port == null ? 8080 : Integer.parseInt(port);
     }
 }
