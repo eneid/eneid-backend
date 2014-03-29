95ea81b5f500924365d14c33af56cf6a93dff60d
Florent Biville <florent.biville@gmail.com>
2014-03-29 21:57:34 +0100

    Add non blocking debug + add explicit password encoder

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mvn clean install && java -jar target/eneid-backend-1.0-SNAPSHOT.jar 
+mvn clean install && java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000 -jar target/eneid-backend-1.0-SNAPSHOT.jar 
