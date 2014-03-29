d2a77234d8994c4b7272af2b6d9a11be509caaa1
Olivier Girardot <ssaboum@gmail.com>
2014-03-29 23:11:03 +0100

    add: push action to timeline

--- src/main/java/io/github/eneid/actions/QuickActionResource.java
+++ src/main/java/io/github/eneid/actions/QuickActionResource.java
@@ -1,17 +1,12 @@
 package io.github.eneid.actions;
 
 import io.github.eneid.auth.AccountsRepository;
-import io.github.eneid.timeline.Message;
 import io.github.eneid.timeline.MessagesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Date;
 
 @RestController
 @RequestMapping("/api/actions")
@@ -46,23 +41,5 @@ public class QuickActionResource {
     public void save(@RequestParam("name") String name) {
         quickActionsRepository.save(new QuickAction(name));
 
-    }
-
-
-    @RequestMapping(
-            value = {"test","test/"},
-            method = RequestMethod.POST
-    )
-    public void putAction(@RequestParam("id") Long id,
-                                       @AuthenticationPrincipal User currentUser) {
-        QuickAction found = quickActionsRepository.findOne(id);
-        if (found != null ){
-            Message message = new Message();
-            message.setAuthor(accountsRepository.findOne(currentUser.getUsername()));
-            message.setContents("");
-            message.setDate(new Date());
-            message.setAction(found);
-            messagesRepository.save(message);
-        }
     }
 }
--- src/main/java/io/github/eneid/timeline/MessagesResource.java
+++ src/main/java/io/github/eneid/timeline/MessagesResource.java
@@ -1,5 +1,8 @@
 package io.github.eneid.timeline;
 
+
+import io.github.eneid.actions.QuickAction;
+import io.github.eneid.actions.QuickActionsRepository;
 import io.github.eneid.auth.AccountsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
@@ -17,11 +20,15 @@ public class MessagesResource {
 
     private final MessagesRepository messagesRepository;
     private final AccountsRepository accountRepository;
+    private final QuickActionsRepository quickActionRepository;
 
     @Autowired
-    public MessagesResource(MessagesRepository messagesRepository, AccountsRepository accountRepository) {
+    public MessagesResource(MessagesRepository messagesRepository,
+                            AccountsRepository accountRepository,
+                            QuickActionsRepository quickActionRepository) {
         this.messagesRepository = messagesRepository;
         this.accountRepository = accountRepository;
+        this.quickActionRepository = quickActionRepository;
     }
 
     @RequestMapping(value = {"timeline", "timeline/"},
@@ -41,5 +48,22 @@ public class MessagesResource {
         message.setAuthor(accountRepository.findOne(currentUser.getUsername()));
         message.setContents(contents);
         messagesRepository.save(message);
+    }
+
+    @RequestMapping(
+            value = {"timeline/action","timeline/action/"},
+            method = RequestMethod.POST
+    )
+    public void putAction(@RequestParam("id") Long id,
+                          @AuthenticationPrincipal User currentUser) {
+        QuickAction found = quickActionRepository.findOne(id);
+        if (found != null ){
+            Message message = new Message();
+            message.setAuthor(accountRepository.findOne(currentUser.getUsername()));
+            message.setContents("");
+            message.setDate(new Date());
+            message.setAction(found);
+            messagesRepository.save(message);
+        }
     }
 }
