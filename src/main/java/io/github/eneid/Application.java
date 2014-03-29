ebda0e3180a3f382ec7f4aeb46e4b6e9e410029a
Olivier Girardot <ssaboum@gmail.com>
2014-03-29 11:30:07 +0100

    add: run configuration for main

--- src/main/java/io/github/eneid/Application.java
+++ src/main/java/io/github/eneid/Application.java
@@ -1,8 +1,18 @@
 package io.github.eneid;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@EnableAutoConfiguration
 public class Application {
 
+    /**
+     * Main entry point for web app
+     * @param args
+     */
     public static void main(String[] args) {
-        //TODO
+        SpringApplication.run(Application.class, args);
     }
 }
---
+++ src/main/java/io/github/eneid/auth/LoginResource.java
@@ -0,0 +1,7 @@
+package io.github.eneid.auth;
+
+/**
+ * Created by ogirardot on 29/03/2014.
+ */
+public class LoginResource {
+}
