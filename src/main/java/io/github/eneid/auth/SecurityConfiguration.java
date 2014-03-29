e0a0a81ee3fd01d0984c035fa219fee9459e6097
Olivier Girardot <ssaboum@gmail.com>
2014-03-29 22:11:10 +0100

    fix: options not allowed when cors

--- src/main/java/io/github/eneid/auth/SecurityConfiguration.java
+++ src/main/java/io/github/eneid/auth/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import com.googlecode.flyway.core.Flyway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -22,11 +23,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/**").permitAll();
+        
         http.csrf().disable().authorizeRequests()
                 .antMatchers("/api/**").hasRole("USER")
                 .anyRequest().anonymous()
                 .and()
                 .httpBasic();
+
     }
 
     @Bean
