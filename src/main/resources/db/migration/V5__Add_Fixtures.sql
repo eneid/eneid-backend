140dc031e4ccfc14c90fec343718bd8b5681d8e9
Bogdan Soos <bogdan.soos@gmail.com>
2014-03-30 09:46:33 +0200

    Update V5__Add_Fixtures.sql
    Correct spelling error

--- src/main/resources/db/migration/V5__Add_Fixtures.sql
+++ src/main/resources/db/migration/V5__Add_Fixtures.sql
@@ -4,5 +4,5 @@ insert into actions values (1, 'Je viens de le voir');
 insert into actions values (2, 'Je viens de l''appeler');
 insert into actions values (3, 'Il a fait une activité');
 insert into actions values (4, 'Il faudrait l''appeler');
-insert into actions values (5, 'Il a plus d''argent');
+insert into actions values (5, 'Il n''a plus d''argent');
 
